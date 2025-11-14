b26eb6a7b54017b14a091060a44ceaab4549c495
Akula Sandeep Kumar <159024763+SandeepKumarAkula@users.noreply.github.com>
2025-11-14 16:28:36 +0530

    Delete AIISARS directory

--- AIISARS/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import("next").NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // fixes Render warnings
-  },
-  output: "standalone", // allows clean production build
-};
-
-export default nextConfig;
